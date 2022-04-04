66828774dd5f1b7eab335c8da6e886c6e6543614
AdrianDelaCruz1 <adrian.delacruz@studnts.mq.edu.au>
2022-04-04 20:58:33 +1000

    updated Client file

--- src/pre-compiled/MyClient.java
+++ src/pre-compiled/MyClient.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.net.*;
 
 //./ds-server -c ./ds-sample-config01.xml -v brief -n
+//./demoS1.sh MyClient.class -n
 
 public class MyClient {
     public static void main(String[] args) {
@@ -10,10 +11,10 @@ public class MyClient {
             String[] arrServer = { "0", "0", "0", "0", "0", "0", "0", "0", "0" };
             int maxCores = 0;
             int jobID = 0;
+            int servID = 0;
             Integer nRecs;
             String largestServType = "";
             int nServers = 0;
-            // ArrayList<String> serverType = new ArrayList<String>();
 
             Socket s = new Socket("127.0.0.1", 50000);
             BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
@@ -23,16 +24,16 @@ public class MyClient {
             String str = (String) in.readLine();
             System.out.println(str);
 
-            dout.write(("AUTH Adrian\n").getBytes()); // Authentication
+            dout.write(("AUTH adrian\n").getBytes()); // Authentication
             str = (String) in.readLine();
             System.out.println(str);
 
-            while (!(str.contains("NONE"))) {
+            while (!(str.contains("NONE"))) { // Check that the last msg is not NONE
                 dout.write(("REDY\n").getBytes());
                 str = (String) in.readLine();
                 System.out.println(str);
 
-                while (str.contains("JCPL")) {
+                while (str.contains("JCPL")) { // try to change
                     dout.write(("REDY\n").getBytes());
                     str = (String) in.readLine();
                     System.out.println(str);
@@ -61,30 +62,35 @@ public class MyClient {
                         maxCores = numCores;
                         largestServType = arrServer[0];
                         nServers = 1;
-                    } else if (numCores == maxCores) {
+                    } else if (largestServType.equals(arrServer[0])) {
                         nServers++;
                     }
+                    System.out.println(
+                            "Largest server type: " + largestServType + ", and no. of servers are: " + nServers);
                 }
-
-                System.out.println(
-                        "The largest server type is: " + largestServType + ", and the no. of servers are: " + nServers);
 
                 dout.write(("OK\n").getBytes());
                 str = (String) in.readLine();
                 System.out.println(str);
 
-                dout.write(("SCHD " + jobID + " " + largestServType + " " + arrServer[1] + "\n").getBytes());
+                dout.write(("SCHD " + jobID + " " + largestServType + " " + servID + "\n").getBytes()); // schd'ing
+                System.out.println("SCHD " + jobID + " " + largestServType + " " + jobID % nServers + "\n");
                 str = (String) in.readLine();
                 System.out.println(str);
-
+                if (servID == nServers) {
+                    servID = 0;
+                }
                 jobID++;
+                nServers = 0;
             }
 
             dout.write(("QUIT\n").getBytes());
             dout.close();
             s.close();
 
-        } catch (Exception e) {
+        } catch (
+
+        Exception e) {
             System.out.println(e);
         }
     }
